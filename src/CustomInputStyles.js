97c10c1cbee12ccff1a0339e8d0f888dfdf53f45
Maleesha Gimshan <47087449+maleeshagimshan98@users.noreply.github.com>
2023-08-03 01:22:51 +0530

    Changed styles class properties into new format
    modified the component to align with the CustomInputStyle Class's new format

--- src/CustomInputStyles.js
+++ src/CustomInputStyles.js
@@ -3,71 +3,68 @@
  */
 
 class CustomInputStyles {
-
-    /**
-     * 
-     * @param {*} input 
-     * @param {*} label 
-     * @param {*} error 
-     */
-    constructor ({input,label,errorMessage, successMessage})
-    {
-        this.input = {
-            primary : [],
-            focused : [],
-            error  : [],
-            success : []
-        }
-        this.label = []
-        this.errorMessage = []
-        this.successMessage = []
-
-        this._init({input,label,errorMessage, successMessage})
+  /**
+   *
+   * @param {Object} input
+   * @param {Object} label
+   * @param {Object} message
+   */
+  constructor({ input, label, message }) {
+    this.input = {
+      primary: [],
+      focused: [],
+      error: [],
+      success: [],
     }
-
-    /**
-     * Check if required parameters are in proper form and
-     * append the parameters into an array (if a string passed)
-     * 
-     * @param {*} style
-     * @returns {*}
-     * @throws {Error}
-     */
-    _checkParamTypesAndPrepare (style)
-    {    
-        if (Array.isArray(style)) {
-            return style
-        }
-        if (typeof style == "string") {
-            return [style]
-        }
-        else {
-            throw new Error('Type of input style is not supported')
-        }
+    this.label = {
+      primary: [],
+      error: [],
+      success: [],
     }
-
-    /**
-     * initialize styles
-     * 
-     * @param {string} input 
-     * @param {string} label 
-     * @param {string} error 
-     */
-    _init ({input,label,errorMessage,successMessage})
-    {
-        if (Object.keys(input).length > 0) {
-            for (let prop in input) {
-                if (!input[prop]) {
-                    continue
-                }
-                this.input[prop] = this._checkParamTypesAndPrepare(input[prop])
-            }
-        }
-        
-        this.label = this._checkParamTypesAndPrepare(label)
-        this.errorMessage = this._checkParamTypesAndPrepare(errorMessage)  
-        this.successMessage = this._checkParamTypesAndPrepare(successMessage)      
+    this.message = {
+      primary: [],
+      error: [],
+      success: [],
     }
+    this._init({ input, label, message })
+  }
+
+  /**
+   * Check if required parameters are in proper form and
+   * append the parameters into an array (if a string passed)
+   *
+   * @param {*} style
+   * @returns {Array | String}
+   * @throws {Error}
+   */
+  _checkParamTypesAndPrepare(style) {
+    if (Array.isArray(style)) {
+      return style
+    }
+    if (typeof style == "string") {
+      return [style]
+    } else {
+      throw new Error("Type of input style is not supported")
+    }
+  }
+
+  /**
+   * initialize styles
+   *
+   * @param {Object} input
+   * @param {Object} label
+   * @param {Object} message
+   */
+  _init(styles) {
+    ["input", "label", "message"].forEach((element) => {
+      for (let prop in styles[element]) {
+        if (!styles[element][prop]) {
+          continue
+        }
+        this[element][prop] = this._checkParamTypesAndPrepare(styles[element][prop])
+      }
+    })
+  }
 }
 
-export default CustomInputStyles;
+export default CustomInputStyles
