5a14a583c88dbc89803b1fd60a55e3c2633f75b5
maleeshagimshan98 <maleeshagimshan74@gmail.com>
2023-09-21 21:13:47 +0530

    fixed syntax error

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import CustomInput from("./CustomInput.vue")
-import CustomInputState from("./CustomInputState")
-import CustomInputGroupController from ("./CustomInputGroupController")
-import CustomInputStyles from("./CustomInputStyles")
+import CustomInput from "./CustomInput.vue"
+import CustomInputState from "./CustomInputState"
+import CustomInputGroupController from "./CustomInputGroupController"
+import CustomInputStyles from "./CustomInputStyles"
 
 export default {CustomInput,CustomInputState, CustomInputGroupController, CustomInputStyles}
