d08b7fc6cc2a1ae274773a1a4d9dfb3869d6d722
maleeshagimshan98 <47087449+maleeshagimshan98@users.noreply.github.com>
2024-02-24 01:06:06 +0530

    -renamed the class names

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import CustomInput from "./CustomInput.vue"
+import CustomTextInput from "./CustomInput.vue"
 import CustomInputState from "./CustomInputState"
-import CustomInputGroupController from "./CustomInputGroupController"
+import CustomTextInputGroupController from "./CustomInputGroupController"
 import CustomInputStyles from "./CustomInputStyles"
 
-export {CustomInput,CustomInputState, CustomInputGroupController, CustomInputStyles}
+export {CustomTextInput as CustomInput,CustomInputState, CustomTextInputGroupController, CustomInputStyles}
