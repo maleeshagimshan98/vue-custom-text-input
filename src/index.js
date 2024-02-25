d3bb1f23df548da9531a367445b395bb768589de
maleeshagimshan98 <47087449+maleeshagimshan98@users.noreply.github.com>
2024-02-26 02:16:42 +0530

    - Solution for issue #10

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import CustomTextInput from "./CustomInput.vue"
 import CustomInputState from "./CustomInputState"
 import CustomTextInputGroupController from "./CustomInputGroupController"
 import CustomInputStyles from "./CustomInputStyles"
+import Utils from "./Utils"
 
-export {CustomTextInput as CustomInput,CustomInputState, CustomTextInputGroupController, CustomInputStyles}
+export {CustomTextInput as CustomInput,CustomInputState, CustomTextInputGroupController, CustomInputStyles, Utils,}
