669a505d7932c0254c2958d5f6056b60bc94700c
maleeshagimshan98 <47087449+maleeshagimshan98@users.noreply.github.com>
2025-06-10 22:24:16 +0530

    Add VALIDATOR_NOT_DEFINED error type and message to StateError

--- src/Errors/StateError.ts
+++ src/Errors/StateError.ts
@@ -1,6 +1,7 @@
 enum StateErrorType {
   'STATE_NOT_SET',
   'STATE_ALREADY_EXIST',
+  'VALIDATOR_NOT_DEFINED',
 }
 
 const errorMessage = {
@@ -9,6 +10,9 @@ const errorMessage = {
   },
   [StateErrorType.STATE_ALREADY_EXIST]: (name: string): string => {
     return `CustomTextInputGroupController - An input component with name ${name} is already exists.`;
+  },
+  [StateErrorType.VALIDATOR_NOT_DEFINED]: (name: string): string => {
+    return `Validation failed in '${name}': A validator is not defined. Please provide a validator during initialization.`;
   },
 };
 
