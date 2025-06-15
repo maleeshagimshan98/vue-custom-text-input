7f7d94e0357e02584efbf250149306355fc91e60
maleeshagimshan98 <47087449+maleeshagimshan98@users.noreply.github.com>
2025-06-15 15:11:10 +0530

    deleted .js files

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import CustomTextInput from "./CustomInput.vue"
-import CustomInputState from "./CustomInputState"
-import CustomTextInputGroupController from "./CustomInputGroupController"
-import CustomInputStyles from "./CustomInputStyles"
-import Utils from "./Utils"
-
-export {CustomTextInput as CustomInput,CustomInputState, CustomTextInputGroupController, CustomInputStyles, Utils,}
