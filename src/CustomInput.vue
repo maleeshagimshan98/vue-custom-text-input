06f7d558c75e11f1faffd6a63147d7473926bde4
maleeshagimshan98 <47087449+maleeshagimshan98@users.noreply.github.com>
2025-06-16 01:36:23 +0530

    converted the component to composition API

--- src/CustomInput.vue
+++ src/CustomInput.vue
@@ -5,7 +5,7 @@
     <!-- label-->
     <slot name="label" :state="_state" :controller="controller" :styles="styles">
       <!-- default content -->
-      <p v-bind:class="[styles.label.base, computeStyleObj(styles.label)]" v-if="_state.label">
+      <p v-bind:class="[_styleObj.label.base, computeStyleObj(_styleObj.label)]" v-if="_state.label">
         {{ _state.label }}
       </p>
     </slot>
@@ -21,8 +21,8 @@
         <slot name="leading" :state="_state" :controller="controller" :styles="styles">
         </slot>
         <!-- input element -->
-        <input :type="_state.inputType" :placeholder="_state.placeholder" :disabled="_state.isDisabled()"
-          class="custom-input-el" v-bind:class="[styles.input.base, computeStyleObj(styles.input)]"
+        <input :type="_state.inputType" :placeholder="_state.placeholder" :disabled="_state.isDisabled() || false"
+          class="custom-input-el" v-bind:class="[_styleObj.input.base, computeStyleObj(_styleObj.input)]"
           v-bind:value="controller.getValue(name)" v-on:input="(event) => onInput(event)"
           v-on:focus.stop="(event) => focus(event)" v-on:focusout.stop="(event) => focusOut(event)"
           v-on:keyup.enter.stop="(event) => enter(event)" :ref="'input'" />
@@ -33,180 +33,183 @@
     </div>
     <slot name="message" :state="_state" :controller="controller" :styles="styles">
       <!-- default content -->
-      <p v-bind:class="[styles.message.base, computeStyleObj(styles.message)]" v-if="_state.hasMessages()">
+      <p v-bind:class="[_styleObj.message.base, computeStyleObj(_styleObj.message)]" v-if="_state.hasMessages()">
         <!-- Show Only First Error Message Here -->
-        {{ _state.messages()[0].message }}
+        {{ _state.messages()[0]?.message }}
       </p>
     </slot>
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from "vue"
+<script setup lang="ts">
+// import { defineComponent } from "vue"
 import type { PropType } from "vue"
+import { ref, onBeforeMount, onMounted, defineEmits, } from "vue"
 import { CustomInputState } from "./CustomInputState"
 import type { ValidateCallback } from "./CustomInputState"
 import { CustomInputStyles, InputStyleState } from "./CustomInputStyles"
 import { CustomTextInputGroupController } from "./CustomInputGroupController"
 
-export default defineComponent({
-  name: 'vue-custom-text-input',
-  data: function () {
-    return {
-      //isTyping : false,
-      _state: null as CustomInputState | null,
-    }
-  },
-  props: {
-    name: {
-      type: String,
-      required: true,
-    },
-    controller: {
-      type: Object as PropType<CustomTextInputGroupController>,
-      required: true,
-    },
-    inputType: {
-      type: String,
-      default: "text",
-    },
-    realTimeValidate: {
-      type: Boolean,
-      default: true,
-    },
-    validateCallback: {
-      type: Function as unknown as () => ValidateCallback,
-    },
-    label: {
-      type: String,
-      default: '',
-    },
-    placeholder: {
-      type: String,
-      default: '',
-    },
-    disabled: {
-      type: Boolean,
-      default: false,
-    },
-    initSuccessMsg: {
-      type: String,
-    },
-    initErrorMsg: {
-      type: String,
-    },
-    isReq: {
-      type: Boolean,
-      default: true,
-    },
-    isValid: {
-      type: Boolean,
-      default: true,
-    },
-    resetOnInput: {
-      type: Boolean,
-      default: true,
-    },
-    styles: {
-      type: CustomInputStyles,
-      default: () => {
-        return new CustomInputStyles({
-          input: {
-            base: [],
-            primary: [],
-            focused: [],
-            error: [],
-            success: [],
-          },
-          label: {
-            base: [],
-            primary: [],
-            focused: [],
-            error: [],
-            success: [],
-          },
-          message: {
-            base: [],
-            primary: [],
-            focused: [],
-            error: [],
-            success: [],
-          },
-        })
-      },
-    },
-  },
-  computed: {},
-  components: {},
-  methods: {
-    computeStyleObj(elementStyle: InputStyleState): Record<string, boolean> {
-      return this._state ? {
-        [elementStyle.primary.join(" ")]: this._state.isValid() && !this._state.isSuccess(),
-        [elementStyle.success.join(" ")]: this._state.isSuccess(),
-        [elementStyle.error.join(" ")]: !this._state.isValid(),
-        [elementStyle.focused.join(" ")]: this._state.isFocused(),
-      } : {};
-    },
-    focus(event: FocusEvent) {
-      this.controller.focusByName(this.name)
-      this.$emit("focus", event)
-    },
-    focusOut(event: FocusEvent) {
-      this.controller.setCurrentInputStateFocusOut()
-      this.$emit("focusout", event)
-    },
-    enter(event: KeyboardEvent) {
-      if (this.controller.focusNext()) {
-        this.$emit("enter", event);
-      }
-      else {
-        this.$emit("input-group-complete")
-      }
-    },
-    onInput: function (event: Event) {
-      //this.isTyping = true;
-      if (this.resetOnInput) {
-        this._state?.reset()
-      }
-      if (this.realTimeValidate) {
-        //... validate the input
-        this._state?.validate((event.target as HTMLInputElement)?.value)
-      }
-      this.controller.setData(this.name, (event.target as HTMLInputElement).value)
-      this.$emit("input", (event.target as HTMLInputElement).value)
-    },
-  },
-  beforeMount: async function () {
-    if (this.disabled && this.isReq) {
-      throw new Error(`Cannot set the properties disabled and isReq to true at same time`)
-    }
+const emit = defineEmits(['focus', 'input', 'focusout', 'enter', 'input-group-complete']);
+const input = ref(null);
 
-    //... set the controller
-    if (!this.controller) {
-      throw new Error(`Controller is not injected in component -  ${this.name}`)
-    }
-    this.controller.setState(
-      this.name,
-      new CustomInputState({
-        name: this.name,
-        inputType: this.inputType,
-        label: this.label,
-        placeholder: this.placeholder,
-        realTimeValidate: this.realTimeValidate,
-        // validateCallback: this.validateCallback,
-        disabled: this.disabled,
-        initSuccessMsg: this.initSuccessMsg ?? '',
-        initErrorMsg: this.initErrorMsg ?? '',
-        isReq: this.isReq,
-        isValid: this.isValid,
+const props = defineProps({
+  name: {
+    type: String,
+    required: true,
+  },
+  controller: {
+    type: Object as PropType<CustomTextInputGroupController>,
+    required: true,
+  },
+  inputType: {
+    type: String,
+    default: "text",
+  },
+  realTimeValidate: {
+    type: Boolean,
+    default: true,
+  },
+  validateCallback: {
+    type: Function as unknown as () => ValidateCallback,
+  },
+  label: {
+    type: String,
+    default: '',
+  },
+  placeholder: {
+    type: String,
+    default: '',
+  },
+  disabled: {
+    type: Boolean,
+    default: false,
+  },
+  initSuccessMsg: {
+    type: String,
+  },
+  initErrorMsg: {
+    type: String,
+  },
+  isReq: {
+    type: Boolean,
+    default: true,
+  },
+  isValid: {
+    type: Boolean,
+    default: true,
+  },
+  resetOnInput: {
+    type: Boolean,
+    default: true,
+  },
+  styles: {
+    type: CustomInputStyles,
+    default: () => {
+      return new CustomInputStyles({
+        input: {
+          base: [],
+          primary: [],
+          focused: [],
+          error: [],
+          success: [],
+        },
+        label: {
+          base: [],
+          primary: [],
+          focused: [],
+          error: [],
+          success: [],
+        },
+        message: {
+          base: [],
+          primary: [],
+          focused: [],
+          error: [],
+          success: [],
+        },
       })
-    )
-    this._state = this.controller.getState(this.name)
+    },
   },
-  mounted() {
-    this.controller.setInputRef(this.name, this.$refs.input as HTMLElement)
-  },
-})
+});
+
+const _styleObj = props.styles.getStyles();
+
+const inputState = new CustomInputState({
+  name: props.name,
+  inputType: props.inputType,
+  label: props.label,
+  placeholder: props.placeholder,
+  realTimeValidate: props.realTimeValidate,
+  // validateCallback: props.validateCallback,
+  disabled: props.disabled,
+  initSuccessMsg: props.initSuccessMsg ?? '',
+  initErrorMsg: props.initErrorMsg ?? '',
+  isReq: props.isReq,
+  isValid: props.isValid,
+});
+const _state = ref<CustomInputState>(inputState);
+
+function computeStyleObj(elementStyle: InputStyleState): Record<string, boolean> {
+  return {
+    [elementStyle.primary.join(" ")]: _state.value.isValid() && !_state.value.isSuccess(),
+    [elementStyle.success.join(" ")]: _state.value.isSuccess(),
+    [elementStyle.error.join(" ")]: !_state.value.isValid(),
+    [elementStyle.focused.join(" ")]: _state.value.isFocused(),
+  };
+};
+
+function focus(event: FocusEvent): void {
+  props.controller.focusByName(props.name)
+  emit("focus", event)
+}
+
+function focusOut(event: FocusEvent): void {
+  props.controller.setCurrentInputStateFocusOut()
+  emit("focusout", event)
+}
+
+function enter(event: KeyboardEvent): void {
+  if (props.controller.focusNext()) {
+    emit("enter", event);
+  }
+  else {
+    emit("input-group-complete")
+  }
+}
+
+function onInput (event: Event): void {
+  //this.isTyping = true;
+  if (props.resetOnInput) {
+    _state.value.reset()
+  }
+  if (props.realTimeValidate) {
+    //... validate the input
+    _state.value.validate((event.target as HTMLInputElement)?.value)
+  }
+  props.controller.setData(props.name, (event.target as HTMLInputElement).value)
+  emit("input", (event.target as HTMLInputElement).value)
+}
+
+onBeforeMount(() => {
+  if (props.disabled && props.isReq) {
+    throw new Error(`Cannot set the properties disabled and isReq to true at same time`)
+  }
+
+  //... set the controller
+  if (!props.controller) {
+    throw new Error(`Controller is not injected in component -  ${props.name}`)
+  }
+
+  props.controller.setState(
+    props.name,
+    inputState,
+  )
+});
+
+onMounted(() => {
+  props.controller.setInputRef(props.name, input.value as unknown as HTMLElement)
+});
 </script>
 
 <style scoped>
